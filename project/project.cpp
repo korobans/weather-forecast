ca3899f5255de5e167a470deca13e810157fc121
3614n <orochi13101@gmail.com>
2023-07-19 10:38:47 +0300

    v1.2

--- project/project.cpp
+++ project/project.cpp
@@ -28,6 +28,40 @@ double calculateDistance(double lat1, double lon1, double lat2, double lon2)
     return distance;
 }
 
+double calculateMean(const vector<double>& data) 
+{
+    double sum = 0.0;
+    for (double value : data) 
+    {
+        sum += value;
+    }
+    return sum / data.size();
+}
+
+double calculateTStatistic(const vector<double>& group1, const vector<double>& group2) 
+{
+    double mean1 = calculateMean(group1);
+    double mean2 = calculateMean(group2);
+
+    double variance1 = 0.0;
+    double variance2 = 0.0;
+    for (double value : group1) 
+    {
+        variance1 += pow(value - mean1, 2);
+    }
+    for (double value : group2) {
+        variance2 += pow(value - mean2, 2);
+    }
+    variance1 /= (group1.size() - 1);
+    variance2 /= (group2.size() - 1);
+
+    double numerator = mean1 - mean2;
+    double denominator = sqrt((variance1 / group1.size()) + (variance2 / group2.size()));
+    double t_statistic = numerator / denominator;
+
+    return t_statistic;
+}
+
 int main() 
 {
     vector<string> city;
@@ -36,8 +70,10 @@ int main()
     vector<double> longitude;
     vector<double> height;
     vector<double> length;
-    vector<vector<double>> near;
+    vector<vector<double>> nearleft;
+    vector<vector<double>> nearright;
     double x, y;
+    int k = 0;
 
     sqlite3* db;
     int rc = sqlite3_open("data.sqlite", &db);
@@ -78,18 +114,24 @@ int main()
         double distance = calculateDistance(x, y, latitude[i], longitude[i]);
         if (distance < 500)
         {
+            k++;
             vector<double> row = { temperature[i], latitude[i], longitude[i] , distance};
-            near.push_back(row);
+            if (y >= longitude[i]) nearleft.push_back(row);
+            else nearright.push_back(row);
         }
     }
 
-    for (vector<double> i : near)
-    {
-        for (double e : i)
-        {
-            cout << e << endl;
-        }
+    if ((k>=10)&&(nearleft.size()>=3)&&(nearright.size()>=3))
+    {   
+        double t_statistic = calculateTStatistic(nearleft[0], nearright[0]);
+        int df = nearleft.size() + nearright.size() - 2;
+        double alpha = 0.05;
+        double critical_t = abs(t_statistic) / sqrt((1.0 / nearleft.size()) + (1.0 / nearright.size()));
+
+        if (t_statistic > critical_t) cout << "Использовать осреднение" << endl;
+        else cout << "Использовать интерполяцию" << endl;
     }
+    else if (k < 10) cout << "Воруем у Вадима" << endl;
 
     sqlite3_finalize(stmt);
     sqlite3_close(db);
